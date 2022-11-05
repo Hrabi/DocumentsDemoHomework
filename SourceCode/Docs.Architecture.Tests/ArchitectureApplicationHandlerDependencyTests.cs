e981003a0406dda2d6c5c19e1a06884f164e2dcc
Hrabal, Petr <petr.hrabal@konicaminolta.cz>
2022-11-05 16:46:55 +0100

    Architecture Test - Handler Dependency

---
+++ SourceCode/Docs.Architecture.Tests/ArchitectureApplicationHandlerDependencyTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Docs.Architecture.Tests;
+
+[TestClass]
+public class ArchitectureApplicationHandlerDependencyTests
+{
+  // Writing ArchUnit style tests for .Net and C# to enforce architecture rules
+  // https://www.ben-morris.com/writing-archunit-style-tests-for-net-and-c-for-self-testing-architectures/
+
+  [TestMethod]
+  public void Application_Handler_Should_HaveDependencyOnDomain()
+  {
+    // Arrange
+    var assembly = Application.AssemblyReference.Assembly;
+
+    // Act
+    var testResult = Types.InAssembly(assembly).That().HaveNameEndingWith("Handler").Should().HaveDependencyOn(Namespaces.Domain).GetResult();
+
+    // Assert
+    Assert.IsTrue(testResult.IsSuccessful);
+  }
+}
+
